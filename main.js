f481fa0d5b0619b1e5fd79dbd6a5d071bf515234
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-16 15:31:12 +0200

    Il tasto di chiusura menù hamburger riporta alla situazione di partenza

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-//creo oggetto tasto Hamburger
-var hamburgerButton = $("#hamburgerButton");
-
+//creo oggetto tasto Hamburger e tasto chiusura menù hamburger
+var hamburgerBtn 		= $("#hamburgerBtn");
+var hamburgerBtn__close = $("#hamburgerBtn__close");
+ 
 
 //azione al click di hamburgerButton
-hamburgerButton.click( function(){
-
-	console.log('cliccato');
-
-	//mostro menù hamburger
-	$(".hamburger-menu").show();
-
-	//nascondo menù lungo
+hamburgerBtn.click(function() {
+	//nascondo menù lungo, mostro menù hamburger
 	$(".header-right").hide();
+	$(".hamburger-menu").show("slow");
+});
+
+//azione al click del tasto per chiudere il menù hamburger
+hamburgerBtn__close.click(function() {
+	//nascondo menù hamburger, mostro menù lungo
+	$(".hamburger-menu").hide("slow");
+	$(".header-right").show();
 });
