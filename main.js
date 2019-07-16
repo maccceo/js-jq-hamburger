c414200a5aa43dea2e80b660e74c224ea99a889f
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-16 16:53:10 +0200

    Aggiunto $(document).ready per attendere caricamento completo pagina

--- main.js
+++ main.js
@@ -2,17 +2,20 @@
 var hamburgerBtn 		= $("#hamburgerBtn");
 var hamburgerBtn__close = $("#hamburgerBtn__close");
  
+// aspetta che la pagina sia stata caricata completamente
+$( document ).ready(function() {
+    console.log("documento pronto");
+    //azione al click di hamburgerButton
+    	hamburgerBtn.click(function() {
+    		//nascondo menù lungo, mostro menù hamburger
+    		$(".header-right").hide();
+    		$(".hamburger-menu").show("fast");
+    	});
 
-//azione al click di hamburgerButton
-hamburgerBtn.click(function() {
-	//nascondo menù lungo, mostro menù hamburger
-	$(".header-right").hide();
-	$(".hamburger-menu").show("slow");
-});
-
-//azione al click del tasto per chiudere il menù hamburger
-hamburgerBtn__close.click(function() {
-	//nascondo menù hamburger, mostro menù lungo
-	$(".hamburger-menu").hide("slow");
-	$(".header-right").show();
+    	//azione al click del tasto per chiudere il menù hamburger
+    	hamburgerBtn__close.click(function() {
+    		//nascondo menù hamburger, mostro menù lungo
+    		$(".hamburger-menu").hide("fast");
+    		$(".header-right").show();
+    	});
 });
